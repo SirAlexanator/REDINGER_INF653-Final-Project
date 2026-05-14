5a84f134e0d8515b58603b93dc45f3ea12d4c385
SirAlexanator <aredinger90@gmail.com>
2026-05-13 23:51:46 -0500

    email edit

--- utils/email.js
+++ utils/email.js
@@ -1,14 +1,14 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (to, subject, text) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
+const sendEmail = async (to, subject, text) => {
     await transporter.sendMail({
         from: process.env.EMAIL_USER,
         to,
